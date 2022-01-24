9a302c138c3b5e33646596d54a3f4c0c20c6cf53
Spencer Dent <sd00070@my.westga.edu>
2022-01-24 00:29:28 -0500

    test has letters not in order w/o duplicates

--- tests/tile_rack/test_has_tiles_for.rb
+++ tests/tile_rack/test_has_tiles_for.rb
@@ -12,6 +12,8 @@ class TileRack::TestHasTilesFor < Minitest::Test
     end
 
     def test_rack_has_needed_letters_when_letters_are_not_in_order_no_duplicates
+        @tile_rack.tiles = [:G, :C, :A, :T, :R, :E, :P]
+        assert @tile_rack.has_tiles_for? 'PART'
     end
 
     def test_rack_doesnt_contain_any_needed_letters
