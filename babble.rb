60be583d4734762faa003e86d6fea1c8ecabff80
Spencer Dent <sd00070@my.westga.edu>
2022-01-24 01:43:59 -0500

    populate rack using bag

--- babble.rb
+++ babble.rb
@@ -1,10 +1,12 @@
 require 'spellchecker'
 require 'tempfile'
 
+require_relative 'tile_bag.rb'
 require_relative 'tile_rack.rb'
 
 class Babble
     def initialize
+        @tile_bag = TileBag.new
         @tile_rack = TileRack.new
         @total_score = 0
     end
@@ -12,9 +14,14 @@ class Babble
     def run
         user_input = ''
 
-        until user_input == ':quit'
-            puts "Guess a word: "
+        until (user_input == ':quit' || @tile_bag.empty?)
+            @tile_rack.number_of_tiles_needed().times {@tile_rack.append @tile_bag.draw_tile if !(@tile_bag.empty?)}
+
+            puts "Tiles: #{@tile_rack.tiles.join(', ')}\n\n"
+
+            print 'Guess a word: '
             user_input = gets.chomp
+            puts ''
         end
 
         puts "Thanks for playing, total score: #{@total_score}"
