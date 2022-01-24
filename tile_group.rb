2b09a733ace6ef25e25d3585519e6b16282b36a3
Spencer Dent <sd00070@my.westga.edu>
2022-01-23 21:58:29 -0500

    create TileGroup & Word skeletons

---
+++ tile_group.rb
@@ -0,0 +1,17 @@
+class TileGroup
+
+    attr_accessor :tiles
+
+    def initialize
+        @tiles = []
+    end
+
+    def append tile
+    end
+
+    def remove tile
+    end
+
+    def hand
+    end
+end
