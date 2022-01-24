3afcf73f63f66e3a44b9e2ca76006e912930375c
Spencer Dent <sd00070@my.westga.edu>
2022-01-23 22:28:17 -0500

    create TileGroup#remove test skeleton

---
+++ tests/tile_group/test_remove.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require_relative '../../tile_group.rb'
+
+class TileGroup::TestRemove < Minitest::Test
+    def setup
+        @tile_group = TileGroup.new
+    end
+
+    def test_remove_only_tile
+
+    end
+
+    def test_remove_first_tile_from_many
+
+    end
+
+    def test_remove_last_tile_from_many
+
+    end
+
+    def test_remove_middle_tile_from_many
+
+    end
+
+    def test_remove_multiple_tiles
+
+    end
+
+    def test_make_sure_duplicates_are_not_removed
+
+    end
+end
