368ecf3fef3f263347aa4a369470bf81032d3fa9
Spencer Dent <sd00070@my.westga.edu>
2022-01-23 23:53:23 -0500

    create NumberOfTilesNeeded test skeleton

---
+++ tests/tile_rack/test_number_of_tiles_needed.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require_relative '../../tile_rack.rb'
+
+class TileRack::TestNumberOfTilesNeeded < Minitest::Test
+    def setup
+        @tile_rack = TileRack.new
+    end
+
+    def test_empty_tile_rack_should_need_max_tiles
+    end
+
+    def test_tile_rack_with_one_tile_should_need_max_minus_one_tiles
+    end
+
+    def test_tiles_rack_with_several_tiles_should_need_some_tiles
+    end
+
+    def test_that_full_tile_rack_doesnt_need_any_tiles
+    end
+end
