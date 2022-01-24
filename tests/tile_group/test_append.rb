c26a3d1df6c75af2941d4a67b8794bd40a243f33
Spencer Dent <sd00070@my.westga.edu>
2022-01-23 22:15:37 -0500

    create TileGroup#append test skeleton

---
+++ tests/tile_group/test_append.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require_relative '../../tile_group.rb'
+
+class TestAppend < Minitest::Test
+    def setup
+        @tile_group = TileGroup.new
+    end
+
+    def test_append_one_tile
+
+    end
+
+    def test_append_many_tiles
+
+    end
+
+    def test_append_duplicate_tiles
+
+    end
+end
